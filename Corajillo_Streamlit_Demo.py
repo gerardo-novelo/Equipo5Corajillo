696be8170ecd2c59a9387cac35bd890978b1f02f
gerardo-novelo <73544848+gerardo-novelo@users.noreply.github.com>
2023-03-13 18:58:01 -0600

    Update Corajillo_Streamlit_Demo.py

--- Corajillo_Streamlit_Demo.py
+++ Corajillo_Streamlit_Demo.py
@@ -8,7 +8,7 @@ import plotly.express as px
 def get_data():
 #    url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2022-12-04/visualisations/listings.csv"
 #    return pd.read_csv(url, low_memory=False)
-     return pd.read_csv('DataSetCarajilloDummy.csv, low_memory=False')
+     return pd.read_csv('DataSetCarajilloDummy.csv', low_memory=False)
 
 df = get_data()
 
